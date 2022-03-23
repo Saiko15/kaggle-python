99db28d6e6a2f0728865e6aaab9f10cbfd02a0ae
Ahmed Sayed <69459554+Saiko15@users.noreply.github.com>
2022-03-23 10:28:27 +0200

    hello python

--- hello.py
+++ hello.py
@@ -1,1 +1,52 @@
+color = "blue"
+
+
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+radius = 0.5 * diameter
+
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = pi * (radius ** 2)
+
+
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+
+# Your code goes here. Swap the values to which a and b refer.
+# If you get stuck, you can always uncomment one or both of the lines in
+# the next cell for a hint, or to peek at the solution.
+c = []
+
+c = a
+a = b
+b = c
+
+
+
+#Add parentheses to the following expression so that it evaluates to 1.
+
+(5 - 3) // 2
+
+
+
+#Add parentheses to the following expression so that it evaluates to 0.
+
+(8 - 3) * (2 - (1 + 1))
+
+
+
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+# Your code goes here! Replace the right-hand side of this assignment with an expression
+# involving alice_candies, bob_candies, and carol_candies
+to_smash = (alice_candies + bob_candies + carol_candies) % 3
+
+
+
+
 
