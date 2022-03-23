c41d739a995c1059907e4c73b891d24df83dcb4d
Ahmed Sayed <69459554+Saiko15@users.noreply.github.com>
2022-03-23 15:31:12 +0200

    Lists

--- lists.py
+++ lists.py
@@ -1,1 +1,65 @@
+def select_second(L):
+    """Return the second element of the given list. If the list has no second
+    element, return None.
+    """
+    if len(L) >= 2:
+        return L[1]
+    else:
+        return None
+    pass
+  
+def select_second(L):
+    if len(L) < 2:
+        return None
+    return L[1]
+  
 
+  def losing_team_captain(teams):
+    """Given a list of teams, where each team is a list of names, return the 2nd player (captain)
+    from the last listed team
+    """
+    return teams[-1][1]
+    pass
+
+  
+  
+def purple_shell(racers):
+    """Given a list of racers, set the first place racer (at the front of the list) to last
+    place and vice versa.
+    
+    >>> r = ["Mario", "Bowser", "Luigi"]
+    >>> purple_shell(r)
+    >>> r
+    ["Luigi", "Bowser", "Mario"]
+    """
+    temp = racers[-1]
+    racers[-1] = racers[0]
+    racers[0] = temp
+    
+    
+    
+    
+a = [1, 2, 3]
+b = [1, [2, 3]]
+c = []
+d = [1, 2, 3][1:]
+
+# Put your predictions in the list below. Lengths should contain 4 numbers, the
+# first being the length of a, the second being the length of b and so on.
+lengths = [3,2,0,2]
+
+
+
+def fashionably_late(arrivals, name):
+    """Given an ordered list of arrivals to the party and a name, return whether the guest with that
+    name was fashionably late.
+    """
+    half_guests = len(arrivals) / 2
+    if (arrivals.index(name) >= half_guests) and ( arrivals.index(name) != (len(arrivals) - 1)):
+        return True
+    return False
+  
+  
+def fashionably_late(arrivals, name):
+    order = arrivals.index(name)
+    return order >= len(arrivals) / 2 and order != len(arrivals) - 1
